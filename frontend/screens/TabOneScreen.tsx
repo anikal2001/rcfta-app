875bb4df1761e399d9e82c385bb949f7aca9c7e5
anikal2001 <anirudh@wowa.ca>
2021-06-26 00:00:30 -0400

    Added eslint and tailwind for react native

--- frontend/screens/TabOneScreen.tsx
+++ frontend/screens/TabOneScreen.tsx
@@ -1,23 +1,23 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View} from '../components/Themed';
-import Card from '../components/common/DisplayCard';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import Card from "../components/common/DisplayCard";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-        <Card />
-        <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-      </View>
+      <Card />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
